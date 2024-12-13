f8e69fadfd3147e23e9fc5fb8db1d69846f14edf
Mitrios19 <99819966+Mitrios19@users.noreply.github.com>
2024-12-13 19:07:11 +0700

    pep8

--- blogicum/blog/views.py
+++ blogicum/blog/views.py
@@ -12,7 +12,7 @@ def index(request):
         is_published=True,
         pub_date__lte=now,
         category__is_published=True
-        )
+    )
     paginator = Paginator(posts, 5)
     page_number = request.GET.get('page')
     page_obj = paginator.get_page(page_number)
@@ -23,7 +23,8 @@ def index(request):
 def post_detail(request, post_id):
     post = get_object_or_404(Post.objects.filter(
         is_published=True,
-        category__is_published=True
+        category__is_published=True,
+        pub_date__lte=timezone.now()
     ),
         pk=post_id)
     template = 'blog/detail.html'
